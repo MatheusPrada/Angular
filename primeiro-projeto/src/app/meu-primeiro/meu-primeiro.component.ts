27c0a63c3abadd4f27c8f97d2797fd46cee1abff
Matheus Gugoni Prada <matheus.prada@sp01.local>
2019-11-15 13:31:07 -0300

    Criando os primeiros componentes.

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'meu-primeiro-component' ,
+  template: `
+  <p>Meu primeiro Component com Angular 2.</p>
+  `
+})
+
+export class MeuPrimeiroComponent{ }
